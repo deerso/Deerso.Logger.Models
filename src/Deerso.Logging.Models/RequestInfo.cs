0675632f7783b4fd72dfca3e36f968f5358fb8e1
Kyle Gobel <kgobel@gmail.com>
2014-01-18 21:09:50 -0500

    added RequestInfo

---
+++ src/Deerso.Logging.Models/RequestInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Deerso.Logging.Models
+{
+    public class RequestInfo
+    {
+        public DateTime RequestTime { get; set; }
+        public string UserAgent { get; set; }
+        public bool IsCrawler { get; set; }
+        public string Url { get; set; }
+        public string RefererUrl { get; set; } 
+        public string IpAddress { get; set; }
+        public string OriginRefererUrl { get; set; }
+    }
+}
