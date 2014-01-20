096e5c4156f43da8c2a1cb6efd6d76174398506d
Kyle Gobel <kgobel@gmail.com>
2014-01-19 20:50:06 -0500

    added orderInfo

---
+++ src/Deerso.Logging.Models/OrderInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Deerso.Logging.Models
+{
+    public class OrderInfo
+    {
+        public DateTime TimeOrdered { get; set; } 
+        public int OrderNumber { get; set; }
+        public decimal OrderAmount { get; set; }
+        public string IpAddress { get; set; }
+        public string OriginRefererUrl { get; set; }
+    }
+}
