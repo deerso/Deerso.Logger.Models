155c867803c4d1c42df0f5848228cb12018780e6
Kyle Gobel <kgobel@gmail.com>
2014-01-19 21:00:49 -0500

    added a LiveLogChannel class -- pretty damn clever

---
+++ src/Deerso.Logging.Models/LiveLogChannels.cs
@@ -0,0 +1,22 @@
+﻿namespace Deerso.Logging.Models
+{
+    public sealed class LiveLogChannel
+    {
+        private readonly string _channelName;
+
+        public static LiveLogChannel Debug = new LiveLogChannel("DeersoWebDebug");
+        public static LiveLogChannel Exception = new LiveLogChannel("DeersoWebExceptions"); 
+        public static LiveLogChannel Requests = new LiveLogChannel("DeersoWebRequests");
+        public static LiveLogChannel Orders = new LiveLogChannel("DeersoWebOrders");
+
+        private LiveLogChannel(string channelValue)
+        {
+            _channelName = channelValue;
+        }
+
+        public override string ToString()
+        {
+            return _channelName;
+        }
+    }
+}
